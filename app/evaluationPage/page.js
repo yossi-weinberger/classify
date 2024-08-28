5902daeab543d45d95437b321e1c0c9d2a960cfe
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-08-28 19:27:46 +0300

    Evaluation scrolling fixed

--- app/evaluationPage/page.js
+++ app/evaluationPage/page.js
@@ -1,4 +1,3 @@
-// page.js
 import EvaluationForm from "@/components/EvaluationForm";
 import styles from "./evaluationPage.module.css";
 import Image from "next/image";
@@ -6,51 +5,47 @@ import Image from "next/image";
 export default function EvaluationPage() {
   return (
     <div className={styles.container}>
-      <div className={styles.contentWrapper}>
-        <div className={styles.leftBackground}>
-          <div className={styles.topRightIcon}>
-            <Image
-              src="/icons/V-icon.svg"
-              alt="Evaluation icon"
-              width={120}
-              height={120}
-              className={styles.icon}
-            />
-          </div>
-          <div className={styles.middleRightIcon}>
-            <Image
-              src="/icons/up-graph.svg"
-              alt="Progress icon"
-              width={120}
-              height={120}
-              className={styles.icon}
-            />
-          </div>
-          <div className={styles.bottomRightIcon}>
-            <Image
-              src="/icons/flower.svg"
-              alt="Growth icon"
-              width={120}
-              height={120}
-              className={styles.icon}
-            />
-          </div>
-          <div className={styles.bottomLeftIcon}>
-            <Image
-              src="/icons/about.svg"
-              alt="Info icon"
-              width={120}
-              height={120}
-              className={styles.icon}
-            />
-          </div>
+      <div className={styles.leftBackground}>
+        <div className={styles.topRightIcon}>
+          <Image
+            src="/icons/V-icon.svg"
+            alt="Evaluation icon"
+            width={120}
+            height={120}
+            className={styles.icon}
+          />
         </div>
-        <div className={styles.rightContent}>
-          <div className={styles.title}>טופס הערכת תלמיד</div>
-          <div className={styles.formWrapper}>
-            <EvaluationForm />
-          </div>
+        <div className={styles.middleRightIcon}>
+          <Image
+            src="/icons/up-graph.svg"
+            alt="Progress icon"
+            width={120}
+            height={120}
+            className={styles.icon}
+          />
         </div>
+        <div className={styles.bottomRightIcon}>
+          <Image
+            src="/icons/flower.svg"
+            alt="Growth icon"
+            width={120}
+            height={120}
+            className={styles.icon}
+          />
+        </div>
+        <div className={styles.bottomLeftIcon}>
+          <Image
+            src="/icons/about.svg"
+            alt="Info icon"
+            width={120}
+            height={120}
+            className={styles.icon}
+          />
+        </div>
+      </div>
+      <div className={styles.rightContent}>
+        <div className={styles.title}>טופס הערכת תלמיד</div>
+        <EvaluationForm />
       </div>
     </div>
   );
