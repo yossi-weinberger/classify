7cdf6d728c05a0e16a89e3d3907652328efd8dab
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-09-17 23:09:32 +0300

    new dashboard (SQL)

--- components/dashboard/dashboard.jsx
+++ components/dashboard/dashboard.jsx
@@ -38,7 +38,8 @@ export default function Dashboard() {
         >
           <iframe
             className="dashboard"
-            src="https://lookerstudio.google.com/embed/reporting/9c2b76f3-eac9-46b6-a2e2-8017778ea6b4/page/p_kf9sel82hd"
+            // src="https://lookerstudio.google.com/embed/reporting/9c2b76f3-eac9-46b6-a2e2-8017778ea6b4/page/p_kf9sel82hd"
+            src="https://lookerstudio.google.com/embed/reporting/a6238fd0-fc93-49fe-b22b-024a2b00ba39/page/p_kf9sel82hd"
             frameBorder="0"
             allowFullScreen
             sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
