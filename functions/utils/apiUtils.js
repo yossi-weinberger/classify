353b895408d24c88c18c724a4b478347e84afad4
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-11-10 18:11:16 +0200

    Update apiUtils.js

--- functions/utils/apiUtils.js
+++ functions/utils/apiUtils.js
@@ -3,6 +3,9 @@ export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL; //|| "https://clas
 // export const SERVER_URL = "https://classify-backend.vercel.app";
 
 export async function handleApiRequest(url, options = {}) {
+  console.log("Making API request to:", url);
+  console.log("With options:", options);
+
   try {
     const response = await fetch(url, options);
     console.log("Response status:", response.status);
@@ -16,11 +19,12 @@ export async function handleApiRequest(url, options = {}) {
         const textContent = await response.text();
         errorMessage += ` - ${textContent}`;
       }
+      console.error("API Error:", errorMessage);
       throw new Error(errorMessage);
     }
 
     const data = await response.json();
-    // console.log("Response data:", data);
+    console.log("Response data:", data);
     return data;
   } catch (error) {
     console.error("שגיאת API:", error);
